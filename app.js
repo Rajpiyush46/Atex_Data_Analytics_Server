b90facd460e6d64133ada3e8ba78e74db400feb1
Rajpiyush46 <piyush.raj1@ltts.com>
2026-07-06 16:42:16 +0530

    import api Added

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 import express from "express";
+import cors from "cors";
 import routes from "./src/routes/routes.js";
 
 const app = express();
+// need to check  in which file  it shuld be pleaced  >>> Piyush
+/**
+ * CORS
+ */
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
